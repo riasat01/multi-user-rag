ad0de3a2e52446851328481fbdc5036808bd28ab
riasat01 <riasat01@outlook.com>
2026-09-23 21:12:48 +0600

    helper functions to extract chunks from documents created

---
+++ loaders.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+from langchain_community.document_loaders import PyPDFLoader, TextLoader, Docx2txtLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.schema import Document
+
+from config import CHUNK_SIZE, CHUNK_OVERLAP
+
+SUPPORTED_EXTENSIONS = {".pdf", ".txt", ".docx"}
+
+def load_document(file_path: Path) -> list[Document]:
+    suffix = file_path.suffix.lower()
+
+    if suffix == ".pdf":
+        loader = PyPDFLoader(str(file_path))
+    elif suffix == ".txt":
+        loader = TextLoader(str(file_path), encoding="utf-8")
+    elif suffix == ".docx":
+        loader = Docx2txtLoader(str(file_path))
+    else:
+        raise ValueError(f"Unsupported file type : {suffix}")
+
+    return loader.load()
+
+
+def chunk_documents(docs: list[Document]) -> list[Document]:
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=CHUNK_SIZE,
+        chunk_overlap=CHUNK_OVERLAP,
+    )
+
+    return splitter.split_documents(docs)
+
+def load_and_chunk(file_path: Path) -> list[Document]:
+    docs = load_document(file_path)
+    return chunk_documents(docs)
